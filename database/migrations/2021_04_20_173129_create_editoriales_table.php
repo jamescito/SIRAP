6cc5c7b5f0980d6d0038439db81d5553e29afadc
jamescito <jr76407900@gmail.com>
2021-04-20 11:43:53 -0600

    agregamos pais en editoriales

---
+++ database/migrations/2021_04_20_173129_create_editoriales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEditorialesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('editoriales', function (Blueprint $table) {
+            $table->string('codigo',50);
+            $table->string('editorial',50);
+            $table->string('pais',50);
+            $table->string('correo',50);
+            $table->primary('codigo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('editoriales');
+    }
+}
