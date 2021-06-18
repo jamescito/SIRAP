f0758b1d07b074dea2c80cb9223f787da06d5dd6
jamescito <jr76407900@gmail.com>
2021-06-18 13:48:41 -0600

    sss

--- app/Http/Controllers/API/PrestamosController.php
+++ app/Http/Controllers/API/PrestamosController.php
@@ -32,7 +32,7 @@ class PrestamosController extends Controller
         ->select('libros.titulo','estudiantes.nombre','estudiantes.apellido','users.email')
       //  ->where('estudiantes.id',$id)
         ->get();
-        return response()->json([
+        return response($prestamo)->json([
             'data'=>$prestamo
         ]);
     }
@@ -46,7 +46,9 @@ class PrestamosController extends Controller
         ->select('libros.titulo','estudiantes.nombre','estudiantes.apellido','users.email')
         ->where('users.email',$id)
         ->get();
-        return response($prestamo,200);
+        return response($prestamo)->json([
+            'data'=>$prestamo
+        ]);
     }
 
     
