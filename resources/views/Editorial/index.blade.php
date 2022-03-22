4972af6bf7b5bd36399add985a83c154cddc50e5
jamescito <jr76407900@gmail.com>
2022-03-22 13:24:48 -0600

    Agregamos estilo a la opcion otros

--- resources/views/Editorial/index.blade.php
+++ resources/views/Editorial/index.blade.php
@@ -9,14 +9,14 @@
             <div class=" bg-white overflow-hidden shadow-xl sm:rounded-lg py-3">
                 
             <a href="editoriales/create" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 m-6  border border-blue-500 hover:border-transparent rounded">CREAR</a>
-                 <!--TABLA CON TAILWIND-->
+                <!--TABLA CON TAILWIND-->
                 <div class="my-4 overflow-x-auto sm:mx-6 lg:mx-8 w-full">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                         <thead class="bg-gray-50">
                             <tr>
-                           
+                        
                             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Código
                             </th>
--- resources/views/otros/index.blade.php
+++ resources/views/otros/index.blade.php
@@ -6,4 +6,47 @@
         </h2>
     </x-slot>
 
+
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-1">
+            <div class=" bg-white overflow-hidden shadow-xl sm:rounded-lg py-3">
+    
+            <div class="py-12  bg-blue-100 overflow-hidden shadow-xl sm:rounded-lg mx-auto">
+            <img src="https://www.tecnacional.edu.ni/media/uploads/2016/11/18/logo-inatec-2016.png" alt="" class=" w-60 ">    
+
+                <form action="" method="get" class="mt-9 m-10">
+                    <div class=" ml-14 mt-3">
+                        <label class="ml-14 mt-2 font-bold text-3xl text-blue-600">Otras opciones</label>
+                        <a href="editoriales" class="bg-transparent ml-14 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 m-6  border border-blue-500 hover:border-transparent rounded">Ir a editoriales</a>
+                        <a href="Carreras" class="bg-transparent ml-14 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 m-6  border border-blue-500 hover:border-transparent rounded">Ir a carrera</a>
+                        <a href="autores" class="bg-transparent ml-14 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 m-6  border border-blue-500 hover:border-transparent rounded">Ir a autores</a>
+                        <a href="ares" class="bg-transparent ml-14 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 m-6  border border-blue-500 hover:border-transparent rounded">Ir a areas</a>
+                    </div>
+                </form>
+
+            </div>
+
+    </div>
+        </div>
+            </div>
+
+
+
+
+<!--p>FOOTER</p -->
+
+
+<div class="border-t border-smoke px-8 py-4 bg-white">
+        <div class="flex justify-center text-grey">
+            Todos los derechos reservado 
+        </div>
+        <div class="flex justify-center text-grey">
+            jr76407900@gmail.com
+        </div>
+        <div class="flex justify-center text-grey">
+            Faruckchavarria@gmail.com
+        </div>
+</div>
+
+
 </x-app-layout>
