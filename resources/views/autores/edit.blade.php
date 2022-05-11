c2c56b4a65d4fdc4426e795954801d8920547967
jamescito <jr76407900@gmail.com>
2022-05-11 11:48:01 -0600

    Agregamos estilo a los edit de autores y carreras

--- resources/views/autores/edit.blade.php
+++ resources/views/autores/edit.blade.php
@@ -5,12 +5,14 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="flex flex-col">
-            
-            <form action="\autores/{{ $autor->id}}" class="w-full max-w-lg" method="post">
+    <div class="py-10">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-1">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-3">
+            <div class="py-12 bg-blue-100 overflow-hidden shadow-xl sm:rounded-lg">
+                
+                <img src="https://www.tecnacional.edu.ni/media/uploads/2016/11/18/logo-inatec-2016.png" alt=""  class=" w-60 ">    
+
+            <form action="\autores/{{ $autor->id}}" class="p-5 w-full max-w-lg bg-gray-300  mx-auto  overflow-hidden shadow-xl sm:rounded-lg py-3 -mt-16" method="post">
             @method('PUT')
             @csrf()
             <div class="flex items-center py-2">
@@ -53,4 +55,19 @@
             </div>
         </div>
     </div>
+
+    <!--p>FOOTER</p -->
+
+
+<div class="border-t border-smoke px-8 py-4 bg-white">
+    <div class="flex justify-center text-grey">
+        Todos los derechos reservado 
+    </div>
+    <div class="flex justify-center text-grey">
+        jr76407900@gmail.com
+    </div>
+    <div class="flex justify-center text-grey">
+        Faruckchavarria@gmail.com
+    </div>
+</div>
 </x-app-layout>
